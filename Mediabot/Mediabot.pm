7d77785f8ee40d9e60780e6a17fea66ecaed4281
teuk <teuk@teuk.org>
2023-12-14 15:17:55 -0500

    Twitter

--- Mediabot/Mediabot.pm
+++ Mediabot/Mediabot.pm
@@ -6422,9 +6422,11 @@ sub displayUrlTitle(@) {
 		}
 		#TBD
 	}
-	if ( $sText =~ /x.com/ ) {
-		# Twitter URL = https://x.com/emilyshar1/status/1703268201159876867?s=20
+	if ( ($sText =~ /x.com/) || ($sText =~ /twitter.com/) || ($sChannel =~ /#Phat/i)) {
 		log_message($self,3,"displayUrlTitle() Twitter URL = $sText");
+		my @tAnswers = ( "Ok $sNick, you need to take a breathe", "$sNick the truth is out theeeeere ^^", "You're the wisest $sNick, you checked your sources :P~");
+		botPrivmsg($self,$sChannel,$tAnswers[rand($#tAnswers + 1)]);
+		return undef;
 		my $user;
 		my $id;
 		if ( $sText =~ /^https.*x\.com\/(.*)\/status.*$/ ) {
